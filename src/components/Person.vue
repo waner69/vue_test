e198ec121ab44d4c7edf0614a89fb68ab8e00d9e
lzy <lzy@163.com>
2022-07-01 17:12:33 +0800

    添加Person组件

---
+++ src/components/Person.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>姓名: {{ person.name }}</h1>
+        <h1>地址: {{ person.address }}</h1>
+        <button @click="showName">点击我</button>
+    </div>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: "Person",
+    data() {
+        return {
+            person: {
+                name: "刘子阳",
+                address: "石家庄市",
+            },
+        }
+    },
+    methods: {
+        showName() {
+            alert(this.person.name + "你好");
+        },
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -4,5 +4,7 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
+  el: "#app",
+  // 残缺版的vue不能使用template，用render返回
   render: h => h(App),
-}).$mount('#app')
+});
